95465f7a4f4675a8715180eaa509725a936a3dc5
luisjwhitaker <luisjwhitaker@gmail.com>
2021-07-12 10:43:33 +0800

    Created TaskEntry model and added created a POC view.

--- scholar_task_tracker/slp_logs/models.py
+++ scholar_task_tracker/slp_logs/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class TaskEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_submitted = models.DateTimeField(auto_now_add=True)
+    adventure_slp = models.IntegerField(default=0)
+    arena_slp = models.IntegerField(default=0)
+    quest_slp = models.IntegerField(default=0)
+
+
+    def __str__(self):
+        return f'{self.user} - {self.date_submitted.strftime("%b %d %Y - %H:%M")}'
+
+class Scholar(models.Model):
+    manager_percent = models.IntegerField(default=0)
+    investor_percent = models.IntegerField(default=0)
--- scholar_task_tracker/slp_logs/views.py
+++ scholar_task_tracker/slp_logs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def logs_view(request):
+    taskentry = TaskEntry.objects.all()
+    return render(request,'slp_logs/index.html',{'taskentry':taskentry})
