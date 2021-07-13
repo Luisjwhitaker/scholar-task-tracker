f6063b02faf8a73f8868e030b27d38473911d3c1
luisjwhitaker <luisjwhitaker@gmail.com>
2021-07-13 15:09:08 +0800

    improved TaskEntry model and added Scholar model

--- scholar_task_tracker/slp_logs/models.py
+++ scholar_task_tracker/slp_logs/models.py
@@ -4,14 +4,17 @@ from django.contrib.auth.models import User
 class TaskEntry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date_submitted = models.DateTimeField(auto_now_add=True)
-    adventure_slp = models.IntegerField(default=0)
-    arena_slp = models.IntegerField(default=0)
-    quest_slp = models.IntegerField(default=0)
-
+    adventure_slp = models.PositiveSmallIntegerField(default=0)#,max_value=100)
+    arena_slp = models.PositiveSmallIntegerField(default=0)#,max_value=500)
+    quest_slp = models.PositiveSmallIntegerField(default=0)#,max_value=50)
 
     def __str__(self):
         return f'{self.user} - {self.date_submitted.strftime("%b %d %Y - %H:%M")}'
 
 class Scholar(models.Model):
-    manager_percent = models.IntegerField(default=0)
-    investor_percent = models.IntegerField(default=0)
+    user_nickname = models.CharField(max_length=150)
+    manager_percent = models.PositiveSmallIntegerField(default=0)#,max_value=100)
+    investor_percent = models.PositiveSmallIntegerField(default=0)#,max_value=100)
+
+    def __str__(self):
+        return f'{self.user_nickname}'
