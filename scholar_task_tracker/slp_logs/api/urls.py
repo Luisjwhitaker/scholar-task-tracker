29d6a4472f3149c5ca3fe6355963c1ca2b1eda0b
user.email <luisjwhitaker@gmail.com>
2021-07-29 11:46:51 +0800

    added Full list and CRUD functionality to API.

--- scholar_task_tracker/slp_logs/api/urls.py
+++ scholar_task_tracker/slp_logs/api/urls.py
@@ -4,6 +4,10 @@ from . import views
 app_name = 'scholar_tast_tracker'
 
 urlpatterns = [
-    path('', views.api_task_entry, name='api task page'),
-    path('<str:primery_key>/', views.api_task_entry_specific, name='api task page specific'),
+    path('', views.apiOverview, name='api overview'),
+    path('task-list/', views.api_task, name='api task list'),
+    path('task-detail/<str:pk>/', views.api_task_detail, name='api task detail'),
+    path('task-create/', views.api_task_create, name='api task create'),
+    path('task-update/<str:pk>/', views.api_task_update, name='api task update'),
+    path('task-delete/<str:pk>/', views.api_task_delete, name='api task delete'),
 ]
--- scholar_task_tracker/slp_logs/api/views.py
+++ scholar_task_tracker/slp_logs/api/views.py
@@ -6,9 +6,32 @@ from slp_logs.models import TaskEntry
 from slp_logs.api.serializers import TaskEntrySerializer
 
 @api_view(['GET',])
-def api_task_entry_specific(request,primery_key):
+def apiOverview(request):
+    api_urls = {
+        'List':'/task-list/',
+        'Detail View':'/task-detail/<str:pk>/',
+        'Create':'/task-create/',
+        'Update':'/task-update/<str:pk>/',
+        'Delete':'/task-delete/<str:pk>/',
+    }
+    return Response(api_urls)
+
+@api_view(['GET',])
+def api_task(request):
     try:
-        taskentry = TaskEntry.objects.get(id=primery_key)
+        taskentry = TaskEntry.objects.all()
+    except TaskEntry.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = TaskEntrySerializer(taskentry, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET',])
+def api_task_detail(request,pk):
+    try:
+        taskentry = TaskEntry.objects.get(id=pk)
     except TaskEntry.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
@@ -16,8 +39,35 @@ def api_task_entry_specific(request,primery_key):
         serializer = TaskEntrySerializer(taskentry)
         return Response(serializer.data)
 
-@api_view(['GET',])
-def api_task_entry(request):
-    taskentry = TaskEntry.objects.all()
-    serializer = TaskEntrySerializer(taskentry)
-    return Response(serializer.data)
+@api_view(['POST',])
+def api_task_create(request):
+    if request.method == 'POST':
+        serializer = TaskEntrySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST',])
+def api_task_update(request,pk):
+    try:
+        taskentry = TaskEntry.objects.get(id=pk)
+    except TaskEntry.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'POST':
+        serializer = TaskEntrySerializer(instance=taskentry , data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE',])
+def api_task_delete(request,pk):
+    try:
+        taskentry = TaskEntry.objects.get(id=pk)
+        taskentry.delete()
+    except TaskEntry.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    return Response('Item Deleted')
