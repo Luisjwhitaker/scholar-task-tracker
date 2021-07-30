9ed414f38e0ab833d5a46dea33ad1138c34c0394
user.email <luisjwhitaker@gmail.com>
2021-07-30 16:11:02 +0800

    made it so that api 'user' returned user.username instead of user.id

--- scholar_task_tracker/slp_logs/api/serializers.py
+++ scholar_task_tracker/slp_logs/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from slp_logs.models import *
 
 class TaskEntrySerializer(serializers.ModelSerializer):
+    user = serializers.CharField(source='user.username', read_only=True)
     class Meta:
         model = TaskEntry
         fields = ['user', 'adventure_slp', 'arena_slp', 'quest_slp', 'date_submitted']
