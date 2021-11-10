cf8be6b9731152e502e90a64a2e5b3137f8f2a59
user.email <luisjwhitaker@gmail.com>
2021-11-10 11:08:26 +0800

    intializing heroku commit

--- scholar_task_tracker/slp_logs/api/serializers.py
+++ scholar_task_tracker/slp_logs/api/serializers.py
@@ -3,20 +3,13 @@ from slp_logs.models import *
 
 from django.contrib.auth.models import User
 
-
 class TaskEntrySerializer(serializers.ModelSerializer):
     def getUsername(self, obj):
         return obj.user.username
     username = serializers.SerializerMethodField("getUsername")
     class Meta:
         model = TaskEntry
-        fields = ('username', 'user', 'adventure_slp', 'arena_slp', 'quest_slp', 'date_submitted')
-
-#    def create(self, validated_data):
-#        user = validated_data['user'],
-#        adventure_slp = validated_data['adventure_slp'],
-#        arena_slp = validated_data['arena_slp'],
-#        quest_slp = validated_data['quest_slp'],
+        fields = ('username', 'user', 'adventure_slp', 'arena_slp', 'quest_slp', 'date_submitted', 'id')
 
 class RegisterSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(style={'input_type':'password'},write_only=True)
@@ -45,18 +38,3 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
-
-#    def save(self):
-#        account = User(
-#            email=self.validated_data['email']
-#            username=self.validated_data['username']
-#        )
-#        password = self.validated_data['password']
-#        password2 = self.validated_data['password2']
-#
-#        if password != password2:
-#            raise serializers.ValidationError({'password':'Passwords must match.'})
-#            account.set_password(password)
-#            accound.save()
-#
-#        return User
